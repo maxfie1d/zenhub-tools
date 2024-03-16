d381ddb78189edc70f9d2bae91de3aa7bf37a215
Naoto Ishida <maxfield.walker@gmail.com>
2024-03-16 22:39:23 +0900

    update

--- zenhub/createIssuesToEpic/cli.ts
+++ zenhub/createIssuesToEpic/cli.ts
@@ -13,8 +13,8 @@ const flags = parseArgs(Deno.args, {
 
 const configPath = flags.config;
 const epicUrl = flags["epic-url"];
-const token = flags.token ?? env["ZENHUB_API_TOKEN"];
-const workspaceId = flags["workspace-id"] ?? env["ZENHUB_WORKSPACE_ID"];
+const token = flags.token ?? env["ZENHUB_API_TOKEN"] ?? Deno.env.get("ZENHUB_API_TOKEN");
+const workspaceId = flags["workspace-id"] ?? env["ZENHUB_WORKSPACE_ID"] ?? Deno.env.get("ZENHUB_WORKSPACE_ID"); 
 
 if (!configPath) throw "config undefined";
 if (!epicUrl) throw "epicUrl undefined";
